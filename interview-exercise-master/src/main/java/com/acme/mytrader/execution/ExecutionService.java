1b9c134d228d8227742e2e3a81b284c0f76b9fa7
keyshoreK <keyshore.jobs@gmail.com>
2022-04-04 19:34:08 -0400

    Updated exceptions & logging

--- interview-exercise-master/src/main/java/com/acme/mytrader/execution/ExecutionService.java
+++
@@ -1,6 +0,0 @@
-package com.acme.mytrader.execution;
-
-public interface ExecutionService {
-    void buy(String security, double price, int volume);
-    void sell(String security, double price, int volume);
-}
--- interview-exercise-master/src/main/java/com/acme/mytrader/price/PriceSource.java
+++
@@ -1,6 +0,0 @@
-package com.acme.mytrader.price;
-
-public interface PriceSource {
-    void addPriceListener(PriceListener listener);
-    void removePriceListener(PriceListener listener);
-}
